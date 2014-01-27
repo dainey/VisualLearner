48fc6ef344916edf07f23169d8d288d12ca92007
dainey <daine.gayle@gmail.com>
2014-01-26 19:54:39 -0500

    Testing

--- src/visuallearner/VisualLearner.java
+++ src/visuallearner/VisualLearner.java
@@ -17,7 +17,7 @@ public class VisualLearner {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Testing");
+        System.out.println("Testing the repo");
     }
     
 }
